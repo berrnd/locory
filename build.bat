217daf78f4ae74130b3bbd4871da0f9939651996
Bernd Bestel <bernd@berrnd.de>
2016-12-30 14:42:18 +0100

    Added build stuff

---
+++ build.bat
@@ -0,0 +1,11 @@
+set projectPath=%~dp0
+if %projectPath:~-1%==\ set projectPath=%projectPath:~0,-1%
+
+set releasePath=%projectPath%\.release
+mkdir "%releasePath%"
+
+for /f "tokens=*" %%a in ('type version.txt') do set version=%%a
+
+del "%releasePath%\LOCH_%version%.zip"
+"build_tools\7za.exe" a -r "%releasePath%\LOCH_%version%.zip" "%projectPath%\*" -xr!.* -xr!build_tools -xr!build.bat -xr!composer.json -xr!composer.lock -xr!composer.phar -xr!LOCH.phpproj -xr!LOCH.phpproj.user -xr!LOCH.sln
+"build_tools\7za.exe" d "%releasePath%\LOCH_%version%.zip" components screenshots config.php
